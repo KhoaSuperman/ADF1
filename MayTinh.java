1c85474f176752542b82f71bd7a8c9707706e229
KhoaSuperman <hoanganhkhoa1012@gmail.com>
2014-08-30 10:57:22 +0700

    ADF1 Demo 1

---
+++ MayTinh.java
@@ -0,0 +1,23 @@
+
+public class MayTinh
+{
+    public int sum(int a, int b){
+        int result = a + b;
+        return result;
+    }
+    
+    public int substract(int a, int b){
+        int result = a - b;
+        return result;
+    }
+    
+    public int multiple(int a, int b){
+        int result = a * b;
+        return result;
+    }
+    
+    public float divide(float a, float b){
+        float result = a / b;
+        return result;
+    }
+}
